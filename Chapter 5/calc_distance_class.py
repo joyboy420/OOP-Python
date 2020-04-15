25bd8b22f634dbfd05b0e701790dc34869c21a68
joyboy420 <progress198@gmail.com>
2020-04-15 15:58:16 +0545

    minor change

--- Chapter 5/calc_distance_class.py
+++ Chapter 5/calc_distance_class.py
@@ -17,7 +17,7 @@ class Polygon:
 
     def perimeter(self):
         perimeter = 0
-        points = self.vertices + [self.vertices(0)]
+        points = self.vertices + [self.vertices[0]]
         for i in range(len(self.vertices)):
             perimeter += points[i].distance(points[i+1])
         return perimeter
